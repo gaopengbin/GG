803719cce5fc05a9bd6f35cd23933b3272b8c9c5
gaopengbin <1804287165@qq.com>
2024-09-13 17:56:53 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <n-config-provider :theme="darkTheme">
+    <n-layout>
+      <n-layout-header>颐和园路</n-layout-header>
+      <n-layout-content content-style="padding: 24px;">
+        平山道
+      </n-layout-content>
+      <n-layout-footer>成府路</n-layout-footer>
+    </n-layout>
+    <n-button>Dark Theme</n-button>
+    <HelloWorld msg="Vite + Vue" />
+  </n-config-provider>
 </template>
-
+<script setup lang="ts">
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import { darkTheme } from "naive-ui";
+const theme = ref();
+theme.value = darkTheme;
+</script>
 <style>
 body {
-  font-family: '宋体';
+  font-family: "宋体";
 }
 </style>
