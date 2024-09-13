02bfacf9e1bf9d38071a7284be4d33d074a3b8c4
gaopengbin <1804287165@qq.com>
2024-09-13 23:35:42 +0800

    bg

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,76 @@
 <template>
-  <n-config-provider :theme="darkTheme">
-    <n-layout>
-      <n-layout-header>颐和园路</n-layout-header>
-      <n-layout-content content-style="padding: 24px;">
-        平山道
+  <n-config-provider :theme="theme">
+    <n-layout style="width: 100vw; background-color: transparent">
+      <n-layout-header style="height: 4vh; background-color: transparent">
+        <Header>
+          <n-switch
+            v-model:value="active"
+            size="medium"
+            @update:value="handleChange"
+          >
+            <template #checked-icon>
+              <n-icon :component="Sunny" />
+            </template>
+            <template #unchecked-icon>
+              <n-icon :component="Moon" />
+            </template>
+          </n-switch>
+        </Header>
+      </n-layout-header>
+      <n-layout-content
+        content-style="padding: 24px;height:96vh;background-color: transparent;"
+      >
+        <Home />
       </n-layout-content>
-      <n-layout-footer>成府路</n-layout-footer>
     </n-layout>
-    <n-button>Dark Theme</n-button>
-    <HelloWorld msg="Vite + Vue" />
+    <div class="background"></div>
   </n-config-provider>
 </template>
 <script setup lang="ts">
 import { ref } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import { darkTheme } from "naive-ui";
-const theme = ref();
-theme.value = darkTheme;
+import { GlobalTheme, darkTheme } from "naive-ui";
+import Header from "./views/home/header.vue";
+import Home from "./views/home/index.vue";
+import Sunset from "./assets/wallpaper/sunset.png";
+import Night from "./assets/wallpaper/night.png";
+import { Sunny, Moon } from "@vicons/ionicons5";
+const active = ref(true);
+const theme = ref<GlobalTheme | null>(darkTheme);
+const handleChange = (value: boolean) => {
+  value ? (theme.value = darkTheme) : (theme.value = null);
+  let background: any = document.querySelector(".background");
+  if (value) {
+    background.style.backgroundImage = `url(${Sunset})`;
+  } else {
+    background.style.backgroundImage = `url(${Night})`;
+  }
+};
 </script>
 <style>
+html,
 body {
   font-family: "宋体";
+  margin: 0;
+  padding: 0;
+}
+#app {
+  margin: 0;
+  padding: 0;
+}
+.background {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -1;
+  background-size: cover;
+  background-position: center;
+  /* filter: blur(10px); */
+  background-image: url(./assets/wallpaper/sunset.png);
+  transition: background-image 1s;
+}
+.n-layout {
+  background-color: transparent;
 }
 </style>
---
+++ src/views/home/header.vue
@@ -0,0 +1,54 @@
+<template>
+  <n-split :default-size="0.8">
+    <template #1>
+      <n-menu
+        v-model:value="activeKey"
+        mode="horizontal"
+        :options="menuOptions"
+        responsive
+        @update:value="handleUpdateValue"
+        style="height: 100%;"
+      />
+    </template>
+    <template #2>
+      <n-space
+        justify="end"
+        style="height: 100%; align-items: center; padding-right: 10px"
+      >
+        <slot></slot>
+      </n-space>
+    </template>
+  </n-split>
+</template>
+
+<script setup lang="ts">
+import type { Component } from "vue";
+import { h, ref } from "vue";
+import { NIcon } from "naive-ui";
+import type { MenuOption } from "naive-ui";
+// import { RouterLink } from "vue-router";
+import {
+  HomeOutline as HomeIcon,
+} from "@vicons/ionicons5";
+
+function renderIcon(icon: Component) {
+  return () => h(NIcon, null, { default: () => h(icon) });
+}
+
+const activeKey = ref("home");
+
+const menuOptions: MenuOption[] = [
+  {
+    label: "首页",
+    key: "home",
+    icon: renderIcon(HomeIcon),
+  },
+];
+// const message = useMessage();
+function handleUpdateValue(key: string, item: MenuOption) {
+  // message.info(`[onUpdate:value]: ${JSON.stringify(key)}`);
+  // message.info(`[onUpdate:value]: ${JSON.stringify(item)}`);
+}
+</script>
+
+<style scoped></style>
